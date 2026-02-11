9b8f2b38ad2e8c7101b890f2a3e1b542281f7a24
Knight Robotics <frc6901@gmail.com>
2026-02-10 18:44:54 -0600

    refactor code - create constants for drivestation

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,12 +4,9 @@
 
 package frc.robot;
 
-import static edu.wpi.first.units.Units.MetersPerSecond;
-import static edu.wpi.first.units.Units.RadiansPerSecond;
-import static edu.wpi.first.units.Units.RotationsPerSecond;
-
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.ctre.phoenix6.swerve.SwerveRequest.FieldCentric;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -22,11 +19,6 @@ import frc.robot.Constants.*;
 import frc.robot.subsystems.*;
 
 public class RobotContainer {
-    private double MaxSpeed = 1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top
-                                                                                        // speed
-    private double MaxAngularRate = RotationsPerSecond.of(0.1).in(RadiansPerSecond); // 3/4 of a rotation per second max
-                                                                                     // angular velocity
-
     /* Setting up bindings for necessary control of the swerve drive platform */
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
             .withDeadband(ControllerConstants.kDeadband).withRotationalDeadband(ControllerConstants.kDeadband) // Add a
@@ -36,7 +28,7 @@ public class RobotContainer {
     private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
     private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
 
-    private final Telemetry logger = new Telemetry(MaxSpeed);
+    private final Telemetry logger = new Telemetry(DrivetrainConstants.MaxSpeed);
 
     private final CommandXboxController driver = new CommandXboxController(ControllerConstants.kDriverPort);
     private final CommandXboxController operator = new CommandXboxController(ControllerConstants.kOperatorPort);
@@ -52,31 +44,12 @@ public class RobotContainer {
     }
 
     private void configureBindings() {
-        // Note that X is defined as forward according to WPILib convention,
-        // and Y is defined as to the left according to WPILib convention.
-        drivetrain.setDefaultCommand(
-                // Drivetrain will execute this command periodically
-                drivetrain.applyRequest(() -> {
-                    double translationX = 0;
-                    double translationY = 0;
-                    double angularRotation = 0;
-
-                    // only move if the trigger is pressed
-                    if (driver.rightBumper().getAsBoolean()) {
-                        translationX = -driver.getLeftY() / 8 * MaxSpeed;
-                        translationY = -driver.getLeftX() / 8 * MaxSpeed;
-                        angularRotation = driver.getRightX() * MaxAngularRate;
-                    }
-
-                    return drive.withVelocityX(translationX) // Drive forward with negative Y (forward)
-                            .withVelocityY(translationY) // Drive left with negative X (left)
-                            .withRotationalRate(angularRotation) // Drive counterclockwise with negative X (left)
-                    ;
-                }));
+        drivetrain.setDefaultCommand(drivetrain.applyRequest(() -> getDriverInput()));
 
         // Idle while the robot is disabled. This ensures the configured
         // neutral mode is applied to the drive motors while disabled.
         final var idle = new SwerveRequest.Idle();
+
         RobotModeTriggers.disabled().whileTrue(
                 drivetrain.applyRequest(() -> idle).ignoringDisable(true));
 
@@ -95,6 +68,26 @@ public class RobotContainer {
         driver.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
 
         drivetrain.registerTelemetry(logger::telemeterize);
+    }
+
+    // Generates the command request for moving the drive train based on the current
+    // controller input.
+    public FieldCentric getDriverInput() {
+        double translationX = 0;
+        double translationY = 0;
+        double angularRotation = 0;
+
+        // only move if the trigger is pressed
+        if (driver.rightBumper().getAsBoolean()) {
+            translationX = -driver.getLeftY() * DrivetrainConstants.MaxSpeed;
+            translationY = -driver.getLeftX() * DrivetrainConstants.MaxSpeed;
+
+            angularRotation = driver.getRightX() * DrivetrainConstants.MaxAngularRate;
+        }
+
+        return drive.withVelocityX(translationX) // Drive forward with negative Y (forward)
+                .withVelocityY(translationY) // Drive left with negative X (left)
+                .withRotationalRate(angularRotation); // Drive counterclockwise with negative X (left)
     }
 
     public Command getAutonomousCommand() {
