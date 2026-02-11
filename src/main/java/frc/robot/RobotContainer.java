f0420f630dda9f1ed2ea9a5897b5d71d457bd2d7
Knight Robotics <frc6901@gmail.com>
2026-02-10 18:12:14 -0600

    some better parameter tuning for drive code (only move on right trigger)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,18 +18,17 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 // import frc.robot.generated.TunerConstants;
-import frc.robot.Constants.ControllerConstants;
-import frc.robot.Constants.TunerConstants;
-import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.Constants.*;
+import frc.robot.subsystems.*;
 
 
 public class RobotContainer {
     private double MaxSpeed = 1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
-    private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+    private double MaxAngularRate = RotationsPerSecond.of(0.1).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
     /* Setting up bindings for necessary control of the swerve drive platform */
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
-            .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+            .withDeadband(ControllerConstants.kDeadband).withRotationalDeadband(ControllerConstants.kDeadband) // Add a 10% deadband
             .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
     private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
     private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
@@ -54,10 +53,23 @@ public class RobotContainer {
         // and Y is defined as to the left according to WPILib convention.
         drivetrain.setDefaultCommand(
             // Drivetrain will execute this command periodically
-            drivetrain.applyRequest(() ->
-                drive.withVelocityX(-driver.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
-                    .withVelocityY(-driver.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-                    .withRotationalRate(-driver.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
+            drivetrain.applyRequest(() -> {
+                    double translationX = 0;
+                    double translationY = 0;
+                    double angularRotation = 0;
+
+                    // only move if the trigger is pressed
+                    if (driver.rightBumper().getAsBoolean()) {
+                        translationX = -driver.getLeftY()/8 * MaxSpeed;
+                        translationY = -driver.getLeftX()/8 * MaxSpeed;
+                        angularRotation = driver.getRightX() * MaxAngularRate;
+                    }
+
+                    return drive.withVelocityX(translationX) // Drive forward with negative Y (forward)
+                        .withVelocityY(translationY) // Drive left with negative X (left)
+                        .withRotationalRate(angularRotation) // Drive counterclockwise with negative X (left)
+;
+                }
             )
         );
 
